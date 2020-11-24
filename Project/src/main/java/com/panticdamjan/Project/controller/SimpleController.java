4ce82b7d3667d4bc146061edb92041dc0b75fd32
DamjanPantic <panticdamjan@gmail.com>
2020-11-24 22:42:35 +0100

    environment variable added

--- Project/src/main/java/com/panticdamjan/Project/controller/SimpleController.java
+++ Project/src/main/java/com/panticdamjan/Project/controller/SimpleController.java
@@ -1,13 +1,18 @@
 package com.panticdamjan.Project.controller;
 
 import com.panticdamjan.Project.service.SimpleService;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "")
@@ -18,8 +23,14 @@ public class SimpleController {
 
     @GetMapping(path = "", produces = "application/json")
     public ResponseEntity getUser() {
-
         int counter = simpleService.incrementCounter();
-        return new ResponseEntity<>(counter, HttpStatus.OK);
+
+        Map<String, Object> result = new HashMap<String,Object>();
+        result.put("counter", counter);
+        result.put("url", System.getenv("url"));
+        result.put("username", System.getenv("username"));
+        result.put("password", System.getenv("password"));
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
